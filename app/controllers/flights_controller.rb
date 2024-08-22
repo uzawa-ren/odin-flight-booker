8acb6001ac1c5d6a5732b5bf17bf95cd2bf8d9dc
Uzawa Ren <96781771+uzawa-ren@users.noreply.github.com>
2024-08-22 20:25:00 +0000

    Add dropdown search filters for flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,11 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.all
+    @departure_dates = Flight.select(:departure_date)
+                             .distinct
+                             .order(:departure_date)
+                             .map { |flight| [flight.departure_date.strftime("%m/%d/%Y"),
+                                              flight.departure_date]}
+                             .uniq{ |dates_array| dates_array.first }
   end
 end
