fb7a8c99fe7810dc28ec431e6450679b9a38eb1a
Uzawa Ren <96781771+uzawa-ren@users.noreply.github.com>
2024-08-22 14:14:11 +0000

    Seed database with flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,74 @@
 ["ZTR", "ODS", "LWO", "IEV", "VIN", "DNK", "FUK", "KUH", "HND", "KIX"].each do |airport_code|
   Airport.find_or_create_by!(code: airport_code)
 end
+
+[
+  {
+    departure_date: DateTime.new(2024,9,12, 8,0,0),
+    flight_duration: '10 hours 41 minutes',
+    departure_airport_id: 4,
+    arrival_airport_id: 9
+  },
+  {
+    departure_date: DateTime.new(2024,9,1,  8,0,0),
+    flight_duration: '1 hour 14 minutes',
+    departure_airport_id: 2,
+    arrival_airport_id: 3
+  },
+  {
+    departure_date: DateTime.new(2024,9,3,  12,4,0),
+    flight_duration: '53 minutes',
+    departure_airport_id: 5,
+    arrival_airport_id: 3
+  },
+  {
+    departure_date: DateTime.new(2024,9,3,  19,0,0),
+    flight_duration: '10 hours 44 minutes',
+    departure_airport_id: 7,
+    arrival_airport_id: 3
+  },
+  {
+    departure_date: DateTime.new(2024,9,3,  2,0,0),
+    flight_duration: '10 hours 27 minutes',
+    departure_airport_id: 7,
+    arrival_airport_id: 2
+  },
+  {
+    departure_date: DateTime.new(2024,9,13, 10,0,0),
+    flight_duration: '1 hour 33 minutes',
+    departure_airport_id: 7,
+    arrival_airport_id: 9
+  },
+  {
+    departure_date: DateTime.new(2024,9,4, 6,0,0),
+    flight_duration: '1 hour 1 minute',
+    departure_airport_id: 1,
+    arrival_airport_id: 2
+  },
+  {
+    departure_date: DateTime.new(2024,9,4, 3,0,0),
+    flight_duration: '57 minutes',
+    departure_airport_id: 6,
+    arrival_airport_id: 2
+  },
+  {
+    departure_date: DateTime.new(2024,9,4, 4,0,0),
+    flight_duration: '58 minutes',
+    departure_airport_id: 6,
+    arrival_airport_id: 4
+  },
+  {
+    departure_date: DateTime.new(2024,9,4, 6,0,0),
+    flight_duration: '10 hours 17 minutes',
+    departure_airport_id: 8,
+    arrival_airport_id: 1
+  },
+  {
+    departure_date: DateTime.new(2024,9,4, 7,0,0),
+    flight_duration: '10 hours 16 minutes',
+    departure_airport_id: 10,
+    arrival_airport_id: 6
+  },
+].each do |flight_params|
+  Flight.find_or_create_by!(flight_params)
+end
