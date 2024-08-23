73cc10a5a3808de0a7dac9174aa8344c686aca35
Uzawa Ren <96781771+uzawa-ren@users.noreply.github.com>
2024-08-23 14:44:00 +0000

    Remake search form without using simple_form gem

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,17 @@
 class FlightsController < ApplicationController
   def index
-    @airports = Airport.all
-    @departure_dates = Flight.select(:departure_date)
-                             .distinct
-                             .order(:departure_date)
-                             .map { |flight| [flight.departure_date.strftime("%m/%d/%Y"),
-                                              flight.departure_date]}
-                             .uniq{ |dates_array| dates_array.first }
+    @airports = Airport.all.map { |a| [a.code, a.id] }
+    @departure_dates = departure_dates_formatted_collection
+  end
+
+  private
+
+  def departure_dates_formatted_collection
+    Flight.select(:departure_date)
+          .distinct
+          .order(:departure_date)
+          .map { |f| [f.departure_date.strftime("%m/%d/%Y"),
+                      f.departure_date] }
+          .uniq{ |dates_array| dates_array.first }
   end
 end
