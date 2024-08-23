dbcd829fbd39760f11e67b5e97f5860fb9719b0c
Uzawa Ren <96781771+uzawa-ren@users.noreply.github.com>
2024-08-23 17:48:08 +0000

    Add search results under search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,17 @@
 class FlightsController < ApplicationController
   def index
-    @airports = Airport.all.map { |a| [a.code, a.id] }
+    @airports = airports_formatted_collection
     @departure_dates = departure_dates_formatted_collection
+
+    @flights = find_flights(params[:departure_airport], params[:arrival_airport], params[:departure_date])
   end
 
   private
 
+  def airports_formatted_collection
+    Airport.all.map { |a| [a.code, a.id] }.unshift(" ")
+  end
+  
   def departure_dates_formatted_collection
     Flight.select(:departure_date)
           .distinct
@@ -13,5 +19,13 @@ class FlightsController < ApplicationController
           .map { |f| [f.departure_date.strftime("%m/%d/%Y"),
                       f.departure_date] }
           .uniq{ |dates_array| dates_array.first }
+          .unshift(" ")
+  end
+
+  def find_flights(departure_airport, arrival_airport, departure_date)
+    flights = Flight.all
+    flights = flights.where(departure_airport: departure_airport) if departure_airport.present?
+    flights = flights.where(arrival_airport: arrival_airport) if arrival_airport.present?
+    flights
   end
 end
