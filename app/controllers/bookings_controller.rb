ecbe20874614bf21d7ff6575e8a9e48d1d981050
Uzawa Ren <96781771+uzawa-ren@users.noreply.github.com>
2024-08-27 12:21:45 +0000

    Add booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,30 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:choosen_flight])
+    @number_of_passengers = params[:number_of_passengers]
+    @number_of_passengers.to_i.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      flash[:success] = "You've successfully booked tickets for this flight."
+      redirect_to root_path
+    else
+      flight = Flight.find(params[:booking][:flight_id])
+      passengers_attributes = params[:booking][:passengers_attributes].to_h
+      number_of_passengers = passengers_attributes.size
+      render :new, assigns: { flight: flight,
+                              number_of_passengers: number_of_passengers },
+                   status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id,
+                                    passengers_attributes: [:name, :email])
+  end
 end
